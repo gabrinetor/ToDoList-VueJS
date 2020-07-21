b54197408bacffd7fb96a15703aa07b0724f873f
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-07-21 15:14:22 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container grid-xs py-2">
+      <!-- <img class="img-responsive img-logo" src="0/assets/logo.png" alt="Vue"> -->
+      <form @submit.prevent="addToDo(todo)"> <!--dispara ação para que formulario seja automaticamente atualizado-->
+        <div class="input-group">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Nova tarefa">
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+        <!-- teste {{ todos }} -->
+      </form>
+
+      <div class="todo-list">
+        <todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t" />
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  nome: "app", 
+  components: { Todo }, 
+  data() {
+    return { todos: [], todo: { checked: false } };
+  },
+  methods: {
+    addToDo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false };
+      // console.log(this.todo);
+    },
+
+    toggleTodo(todo){
+      const index = this.todos.findIndex(item => item.id === todo.id);
+      if (index > -1) {  //se houver algum indice
+        const checked = !this.todos[index].checked; //qnd estiver marcado, 
+        this.$set(this.todos, index, { ...this.todos[index], checked })//propriedade do array alterada
+      }
+    },
+
+    removeTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id);
+      if (index > -1) {  
+        this.$delete(this.todos, index);
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .img-logo {
+    max-width: 200px;
+    margin: 0 auto;
+  }
+
+  .todo-list {
+    padding-top: 2rem;  /* 32px; */
+  }
 </style>
